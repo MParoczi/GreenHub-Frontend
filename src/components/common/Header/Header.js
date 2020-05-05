dd6f5e4fede922883a268d6b8777cf26596e67b7
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-05 21:17:47 +0200

    Embed TemporaryDrawer component in Header

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -4,6 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { useStyle } from "./headerStyle";
 import logo from "../../../../public/images/logo.png";
+import TemporaryDrawer from "../TemporaryDrawer/TemporaryDrawer";
 
 function Header() {
   const classes = useStyle();
@@ -14,6 +15,7 @@ function Header() {
         <Toolbar className={classes.toolbar}>
           <img src={logo} alt="legato-logo" className={classes.logo} />
           <Typography className={classes.title}>Legato</Typography>
+          <TemporaryDrawer />
         </Toolbar>
       </AppBar>
     </>
