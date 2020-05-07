4f4218d473e1a931075d2ca86ec4281bb25d58b9
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-07 10:49:33 +0200

    Add ListItems component

---
+++ src/components/common/TemporaryDrawer/ListItems.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider
+} from "@material-ui/core";
+import {
+  HomeTwoTone,
+  VpnKeyTwoTone,
+  LockOpenTwoTone,
+  InfoTwoTone
+} from "@material-ui/icons";
+
+function ListItems({ classes, toggleDrawer }) {
+  return (
+    <div
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        <Link to="/">
+          <ListItem className={classes.listItem}>
+            <ListItemIcon className={classes.icon}>
+              <HomeTwoTone />
+            </ListItemIcon>
+            <ListItemText primary="Home" className={classes.text} />
+          </ListItem>
+        </Link>
+        <Divider />
+        <ListItem>
+          <ListItemIcon className={classes.icon}>
+            <VpnKeyTwoTone />
+          </ListItemIcon>
+          <ListItemText primary="Login" className={classes.text} />
+        </ListItem>
+        <ListItem>
+          <ListItemIcon className={classes.icon}>
+            <LockOpenTwoTone />
+          </ListItemIcon>
+          <ListItemText primary="Registration" className={classes.text} />
+        </ListItem>
+        <Divider />
+        <Link to="/about">
+          <ListItem className={classes.listItem}>
+            <ListItemIcon className={classes.icon}>
+              <InfoTwoTone />
+            </ListItemIcon>
+            <ListItemText
+              primary="About"
+              className={classes.text}
+              classes={{}}
+            />
+          </ListItem>
+        </Link>
+      </List>
+    </div>
+  );
+}
+
+ListItems.propTypes = {
+  classes: PropTypes.object.isRequired,
+  toggleDrawer: PropTypes.func.isRequired
+};
+
+export default ListItems;
