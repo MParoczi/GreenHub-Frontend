cbe6d5b1be8bae7254694411b09a5dfe5527261b
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-07 09:55:31 +0200

    Remove Footer style from App component's style

--- src/components/appStyle.js
+++ src/components/appStyle.js
@@ -1,12 +1,5 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export const useStyle = makeStyles(() => ({
-  root: { minHeight: "100%", position: "relative" },
-  footer: {
-    position: "absolute",
-    bottom: 0,
-    width: "100%",
-    height: "60px",
-    background: "#6cf"
-  }
+  root: { minHeight: "100%", position: "relative" }
 }));
