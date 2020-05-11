9e5a3260b77d4562356928235b9bf71f54d26c5b
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-11 16:47:41 +0200

    Add basic logic for Registration

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import { useStyle } from "./registrationStyle";
+import { validateFirstName } from "./userInputValidation";
+import RegistrationForm from "./RegistrationForm";
 
-function Registration(props) {
-  return <div></div>;
+function Registration() {
+  const classes = useStyle();
+  const [user, setUser] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const formIsValid = () => {
+    const { firstName } = user;
+    const errors = {};
+
+    const firstNameIsValid = validateFirstName(firstName);
+
+    if (firstNameIsValid !== true) errors.firstName = firstNameIsValid;
+
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!formIsValid()) return;
+  };
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setUser(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  return (
+    <Grid container spacing={0} justify="center">
+      <Grid item xs={7} className={classes.row}>
+        <Paper elevation={3} className={classes.paper}>
+          <Typography className={classes.title}>Registration</Typography>
+          <RegistrationForm
+            handleSubmit={handleSubmit}
+            handleChange={handleChange}
+            errors={errors}
+          />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Registration;
