9dcddf18214d93a8937e8422234794991587d530
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-13 08:29:45 +0200

    Increase spacing between inputs

--- src/components/Registration/RegistrationForm.js
+++ src/components/Registration/RegistrationForm.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 function RegistrationForm({ handleSubmit, handleChange, errors, classes }) {
   return (
     <form onSubmit={handleSubmit}>
-      <Grid container spacing={0} justify="center">
+      <Grid container spacing={1} justify="center">
         <Grid item xs={12} lg={6}>
           <TextField
             name="firstName"
