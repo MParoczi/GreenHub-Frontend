56df47f58555e24c0738de985df063b456f8486e
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-13 08:27:29 +0200

    Add Password component

---
+++ src/components/common/Password/Password.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import {
+  IconButton,
+  OutlinedInput,
+  InputLabel,
+  InputAdornment,
+  FormControl,
+  FormHelperText
+} from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import PropTypes from "prop-types";
+
+function Password({ handleChange, error, errorMessage, name, classes }) {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <FormControl variant="outlined" className={classes.textField}>
+      <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+      <OutlinedInput
+        name={name}
+        id="outlined-adornment-password"
+        type={showPassword ? "text" : "password"}
+        onChange={handleChange}
+        error={error}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={handleClickShowPassword}
+              onMouseDown={handleMouseDownPassword}
+              edge="end"
+            >
+              {showPassword ? <Visibility /> : <VisibilityOff />}
+            </IconButton>
+          </InputAdornment>
+        }
+        labelWidth={70}
+      />
+      <FormHelperText error={error}>{errorMessage}</FormHelperText>
+    </FormControl>
+  );
+}
+
+Password.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  error: PropTypes.bool.isRequired,
+  errorMessage: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  classes: PropTypes.object.isRequired
+};
+
+export default Password;
