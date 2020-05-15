c08efc49d80cf0210118f7dba9ccae5345daafb9
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-15 09:59:47 +0200

    Add RegistrationGuide component

---
+++ src/components/Registration/RegistrationGuide.js
@@ -0,0 +1,135 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ImageIcon from "@material-ui/icons/Image";
+import WorkIcon from "@material-ui/icons/Work";
+import BeachAccessIcon from "@material-ui/icons/BeachAccess";
+import Divider from "@material-ui/core/Divider";
+import EmailTwoToneIcon from "@material-ui/icons/EmailTwoTone";
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: "flex",
+    overflow: "scroll",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    margin: theme.spacing(0)
+  },
+  list: {
+    width: "100%"
+  }
+}));
+
+function RegistrationGuide() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={handleOpen}>
+        react-transition-group
+      </button>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <List className={classes.list}>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <ImageIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="First name"
+                  secondary="First name should only contain alphabetical characters"
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <WorkIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Last name"
+                  secondary="Last name should only contain alphabetical characters and apostrophe"
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <EmailTwoToneIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Email"
+                  secondary="The email address have to be a valid one as we send our confirmation letter to that address"
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <BeachAccessIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Password"
+                  secondary="The password should be at least 8 characters long, should contain at least one uppercase letter, one lowercase letter, one number and one special character but should not contain accents"
+                />
+              </ListItem>
+              <Divider variant="inset" component="li" />
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <BeachAccessIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Birth date"
+                  secondary="You have to be at least 14 years old to be able to register"
+                />
+              </ListItem>
+            </List>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+export default RegistrationGuide;
