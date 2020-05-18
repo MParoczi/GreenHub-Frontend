205d2954e8c26fdf0b23c5b01624f051ca07c74d
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-18 13:28:54 +0200

    Fix reducer name in root

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import apiCallsInProgress from "./apiStatusReducer";
-import registrationReducer from "./registrationReducer";
+import countries from "./registrationReducer";
 
 const rootReducer = combineReducers({
   apiCallsInProgress,
-  registrationReducer
+  countries
 });
 
 export default rootReducer;
