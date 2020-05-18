8661a68411eb3efeed05045f90ac91cff93478cb
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-18 06:43:49 +0200

    Clean imports

--- src/components/Registration/RegistrationGuide.js
+++ src/components/Registration/RegistrationGuide.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
+import {
+  Modal,
+  Backdrop,
+  Fade,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Divider
+} from "@material-ui/core";
 import ImageIcon from "@material-ui/icons/Image";
 import WorkIcon from "@material-ui/icons/Work";
 import BeachAccessIcon from "@material-ui/icons/BeachAccess";
-import Divider from "@material-ui/core/Divider";
 import EmailTwoToneIcon from "@material-ui/icons/EmailTwoTone";
 
 const useStyles = makeStyles(theme => ({
