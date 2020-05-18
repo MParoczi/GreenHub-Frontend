4be4d6936274bf168d287dcb9ae8290e251dd8d0
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-18 13:43:39 +0200

    Add home country input field with auto completion

--- src/components/Registration/RegistrationForm.js
+++ src/components/Registration/RegistrationForm.js
@@ -13,17 +13,20 @@ import {
   FormHelperText,
   FormControl
 } from "@material-ui/core";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import { ThemeProvider } from "@material-ui/styles";
 import PropTypes from "prop-types";
 import Password from "../common/Password/Password";
 import { defaultMaterialTheme } from "./registrationStyle";
+import { handleError } from "../../api/apiUtils";
 
 function RegistrationForm({
   handleSubmit,
   handleChange,
   errors,
   classes,
-  user
+  user,
+  countries
 }) {
   return (
     <form onSubmit={handleSubmit}>
@@ -116,6 +119,26 @@ function RegistrationForm({
             </FormControl>
           </ThemeProvider>
         </Grid>
+        <Grid item xs={12}>
+          <Autocomplete
+            id="combo-box-demo"
+            options={countries}
+            getOptionLabel={option => option.name}
+            renderInput={params => (
+              <TextField
+                {...params}
+                name="country"
+                id="country-input"
+                helperText={errors.country}
+                label="Home country"
+                variant="outlined"
+                className={classes.textField}
+                onChange={handleChange}
+                error={errors.country !== undefined}
+              />
+            )}
+          />
+        </Grid>
       </Grid>
       <Box>
         <input id="register-user" type="submit" className={classes.input} />
@@ -138,7 +161,8 @@ RegistrationForm.propTypes = {
   handleChange: PropTypes.func.isRequired,
   errors: PropTypes.object.isRequired,
   classes: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired
+  user: PropTypes.object.isRequired,
+  countries: PropTypes.arrayOf(PropTypes.object).isRequired
 };
 
 export default RegistrationForm;
