250d54a901174b03e45e4ea168ccb7247519467e
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-18 09:49:25 +0200

    Add registrationReducer to root

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import apiCallsInProgress from "./apiStatusReducer";
+import registrationReducer from "./registrationReducer";
 
-const rootReducer = combineReducers({ apiCallsInProgress });
+const rootReducer = combineReducers({
+  apiCallsInProgress,
+  registrationReducer
+});
 
 export default rootReducer;
