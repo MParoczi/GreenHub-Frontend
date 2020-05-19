49628f67ddacc4e0bb66a3965d4b6d485e14870c
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-19 08:13:52 +0200

    Extract formIsValid function to other module

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -2,20 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Grid, Paper, Typography, CircularProgress } from "@material-ui/core";
 import { useStyle } from "./registrationStyle";
-import {
-  validateEmail,
-  validateName,
-  validatePassword,
-  confirmPassword,
-  validateBirthDate,
-  validateCountry
-} from "./userInputValidation";
 import RegistrationForm from "./RegistrationForm";
 import RegistrationGuide from "./RegistrationGuide";
 import {
   loadCountries,
   registerUser
 } from "../../redux/actions/registrationActions";
+import formIsValid from "./userInputValidation";
 import formatToBasicDate from "../../../utils/dateFormatter";
 
 function Registration() {
@@ -31,43 +24,9 @@ function Registration() {
 
   const dispatch = useDispatch();
 
-  const formIsValid = () => {
-    const {
-      firstName,
-      lastName,
-      email,
-      password,
-      confirmedPassword,
-      birthDate,
-      country
-    } = user;
-    const errors = {};
-
-    const firstNameIsValid = validateName(firstName);
-    const lastNameIsValid = validateName(lastName);
-    const emailIsValid = validateEmail(email);
-    const passwordIsValid = validatePassword(password);
-    const passwordIsConfirmed = confirmPassword(password, confirmedPassword);
-    const birthDateIsValid = validateBirthDate(birthDate);
-    const countryIsValid = validateCountry(country, countries);
-
-    if (firstNameIsValid !== true) errors.firstName = firstNameIsValid;
-    if (lastNameIsValid !== true) errors.lastName = lastNameIsValid;
-    if (emailIsValid !== true) errors.email = emailIsValid;
-    if (passwordIsValid !== true) errors.password = passwordIsValid;
-    if (passwordIsConfirmed !== true)
-      errors.confirmedPassword = passwordIsConfirmed;
-    if (birthDateIsValid !== true) errors.birthDate = birthDateIsValid;
-    if (countryIsValid !== true) errors.country = countryIsValid;
-
-    setErrors(errors);
-
-    return Object.keys(errors).length === 0;
-  };
-
   const handleSubmit = event => {
     event.preventDefault();
-    if (!formIsValid()) return;
+    if (!formIsValid(user, countries, setErrors)) return;
     setUser(prevState => ({
       ...prevState,
       birthDate: formatToBasicDate(prevState.birthDate)
