e03a20a2bd37a469b2525381d83877ef824d5768
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-26 18:08:13 +0200

    Change error handling in fetch api to handle different error messages

--- src/api/apiUtils.js
+++ src/api/apiUtils.js
@@ -1,8 +1,8 @@
 export async function handleResponse(response) {
   if (response.ok) return response.json();
   if (response.status === 400) {
-    const error = await response.text();
-    throw new Error(error);
+    const error = await response.json();
+    throw new Error(error.message);
   }
   throw new Error("Network response was not ok.");
 }
