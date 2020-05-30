107d199c88762fca1c3a5e0b0660674a7a08e671
Márk Paróczi <mark.paroczi@gmail.com>
2020-05-30 06:47:03 +0200

    Add basic structure to Login component

---
+++ src/components/Login/Login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { CircularProgress, Grid, Paper, Typography } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import {
+  defaultMaterialTheme,
+  useStyle
+} from "../common/style/registrationLoginStyle";
+import LoginForm from "./LoginForm";
+import { useSelector } from "react-redux";
+
+function Login() {
+  const loading = useSelector(state => state.apiCallsInProgress);
+  const classes = useStyle();
+
+  return (
+    <Grid container spacing={0} justify="center">
+      <Grid item xs={7} className={classes.row}>
+        <Paper elevation={3} className={classes.paper}>
+          <Typography className={classes.title}>Login</Typography>
+          {loading ? (
+            <ThemeProvider theme={defaultMaterialTheme}>
+              <CircularProgress color="secondary" />
+            </ThemeProvider>
+          ) : (
+            <LoginForm />
+          )}
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Login;
