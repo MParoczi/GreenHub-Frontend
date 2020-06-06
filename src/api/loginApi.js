68193f3fc4807457926b5a07ac6db25d3f4d2f51
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-06 09:35:28 +0200

    Set credentials in login fetch

--- src/api/loginApi.js
+++ src/api/loginApi.js
@@ -4,7 +4,8 @@ export function loginUser(user) {
   return fetch("https://localhost:5001/api/account/login", {
     method: "POST",
     headers: { "content-type": "application/json" },
-    body: JSON.stringify(user)
+    body: JSON.stringify(user),
+    credentials: "include"
   })
     .then(handleResponse)
     .catch(handleError);
