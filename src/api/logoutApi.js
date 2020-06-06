a4249dd6c545c1777506ece18e6a58833a74182a
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-06 09:36:04 +0200

    Set credentials in logout fetch

--- src/api/logoutApi.js
+++ src/api/logoutApi.js
@@ -4,7 +4,8 @@ export function logoutUser(user) {
   return fetch("https://localhost:5001/api/account/logout", {
     method: "POST",
     headers: { "content-type": "application/json" },
-    body: JSON.stringify(user)
+    body: JSON.stringify(user),
+    credentials: "include"
   })
     .then(handleResponse)
     .catch(handleError);
