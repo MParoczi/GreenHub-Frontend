2b2e0ff93c7b2b9b2e7ccf0ce38101eb2b57280b
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-09 10:30:54 +0200

    Put loadCountries() call to useCallback

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
@@ -55,11 +55,15 @@ function Registration() {
     }));
   };
 
+  const getCountries = useCallback(() => {
+    dispatch(loadCountries());
+  }, [dispatch]);
+
   useEffect(() => {
     if (countries.length === 0) {
-      dispatch(loadCountries());
+      getCountries();
     }
-  }, []);
+  }, [countries.length, getCountries]);
 
   return (
     <Grid container spacing={0} justify="center">
