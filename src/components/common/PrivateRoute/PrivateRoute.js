f6c5a59984781b41302423acdb76bc7d20523a4b
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-10 13:29:40 +0200

    Change component render condition

--- src/components/common/PrivateRoute/PrivateRoute.js
+++ src/components/common/PrivateRoute/PrivateRoute.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function PrivateRoute({ component: Component, redirect, loggedIn, ...rest }) {
+function PrivateRoute({ component: Component, redirect, user, ...rest }) {
   return (
     <Route
       {...rest}
       render={props =>
-        loggedIn ? <Component {...props} /> : <Redirect to={redirect} />
+        user ? <Component {...props} /> : <Redirect to={redirect} />
       }
     />
   );
@@ -16,7 +16,7 @@ function PrivateRoute({ component: Component, redirect, loggedIn, ...rest }) {
 PrivateRoute.propTypes = {
   component: PropTypes.func.isRequired,
   redirect: PropTypes.string.isRequired,
-  loggedInUser: PropTypes.bool.isRequired
+  user: PropTypes.bool.isRequired
 };
 
 export default PrivateRoute;
