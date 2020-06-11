10fe4190e6ad76e45d4b0ff2b0ffdadb9e4c6116
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-11 16:36:05 +0200

    Add avatar to Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,16 +2,25 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 import { useStyle } from "../common/style/mainStyle";
+import { useSelector } from "react-redux";
+import defaultProfilePic from "../../images/default_profile.png";
 
 function Profile() {
   const classes = useStyle();
+  const user = useSelector(state => state.loggedInUser);
 
   return (
     <Grid container spacing={2} justify="space-evenly">
       <Grid item xs={11} md={4} className={classes.row}>
         <Paper elevation={3} className={classes.paper}>
           <Typography className={classes.title}>My profile</Typography>
+          <Avatar
+            alt="profile-pic"
+            src={user.picture ? user.picture : defaultProfilePic}
+            className={classes.profilePicture}
+          />
         </Paper>
       </Grid>
       <Grid item xs={11} md={6} className={classes.row}>
