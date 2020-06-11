e6306411eb017cceef04954d1bc6eb6a6c50c23d
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-11 16:13:39 +0200

    Add two main paper to Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,26 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { useStyle } from "../common/style/mainStyle";
 
 function Profile() {
-  return <div></div>;
+  const classes = useStyle();
+
+  return (
+    <Grid container spacing={2} justify="space-evenly">
+      <Grid item xs={11} md={4} className={classes.row}>
+        <Paper elevation={3} className={classes.paper}>
+          <Typography className={classes.title}>My profile</Typography>
+        </Paper>
+      </Grid>
+      <Grid item xs={11} md={6} className={classes.row}>
+        <Paper elevation={3} className={classes.paper}>
+          <Typography className={classes.title}>My posts</Typography>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Profile;
