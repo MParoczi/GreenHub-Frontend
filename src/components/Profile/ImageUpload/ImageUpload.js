18895eb37f1bcfbd87f4680c1549c7a15e287a17
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-15 10:47:53 +0200

    Add CloudinaryContext

--- src/components/Profile/ImageUpload/ImageUpload.js
+++ src/components/Profile/ImageUpload/ImageUpload.js
@@ -1,8 +1,19 @@
 import React from "react";
-import cloudinary from "cloudinary-core";
+import { CloudinaryContext } from "cloudinary-react";
+import PropTypes from "prop-types";
 
-function ImageUpload(props) {
-  return <div></div>;
+function ImageUpload({ cloudName, uploadPreset }) {
+  return (
+    <CloudinaryContext
+      cloudName={cloudName}
+      uploadPreset={uploadPreset}
+    ></CloudinaryContext>
+  );
 }
+
+ImageUpload.prototype = {
+  cloudName: PropTypes.string.isRequired,
+  uploadPreset: PropTypes.string.isRequired
+};
 
 export default ImageUpload;
