af9e56338a88e4925e0b520dcea2fc1c69758fff
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-15 15:46:35 +0200

    Add skeleton structure of image upload handling

--- src/components/Profile/ImageUpload/ImageUpload.js
+++ src/components/Profile/ImageUpload/ImageUpload.js
@@ -2,11 +2,28 @@ import React from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import IconButton from "@material-ui/core/IconButton";
 import EditTwoToneIcon from "@material-ui/icons/EditTwoTone";
+import { openUploadWidget } from "./CloudinaryService";
 
-function ImageUpload(props) {
+function ImageUpload() {
+  const options = { cloudName: "dqqo1y0xo", uploadPreset: "h7rcdcwe" };
+
+  const handleUpload = (error, photo) => {
+    if (!error) {
+      if (photo.event === "success") {
+        console.log(photo);
+      }
+    } else {
+      console.log(error);
+    }
+  };
+
+  const uploadImage = () => {
+    openUploadWidget(options, handleUpload);
+  };
+
   return (
     <Tooltip title="Edit profile picture">
-      <IconButton>
+      <IconButton onClick={uploadImage}>
         <EditTwoToneIcon />
       </IconButton>
     </Tooltip>
