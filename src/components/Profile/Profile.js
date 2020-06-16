7d43964d02bd2f5a0cc7cee7b2d35e429272e6b2
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-16 10:18:17 +0200

    Add profile picture viewer

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
+import Viewer from "react-viewer";
 import { useStyle } from "../common/style/mainStyle";
 import { useSelector } from "react-redux";
 import defaultProfilePic from "../../images/default_profile.png";
@@ -11,6 +12,7 @@ import ImageUpload from "./ImageUpload/ImageUpload";
 function Profile() {
   const classes = useStyle();
   const user = useSelector(state => state.loggedInUser);
+  const [visible, setVisible] = useState(false);
 
   return (
     <Grid container spacing={2} justify="space-evenly">
@@ -21,8 +23,28 @@ function Profile() {
             alt="profile-pic"
             src={user.profilePicture ? user.profilePicture : defaultProfilePic}
             className={classes.profilePicture}
+            onClick={() => {
+              setVisible(true);
+            }}
           />
           <ImageUpload />
+          <Viewer
+            visible={visible}
+            onClose={() => {
+              setVisible(false);
+            }}
+            images={[
+              {
+                src: user.profilePicture
+                  ? user.profilePicture
+                  : defaultProfilePic,
+                alt: `Profile picture of ${user.firstName}`
+              }
+            ]}
+            drag={false}
+            noFooter={true}
+            disableMouseZoom={true}
+          />
         </Paper>
       </Grid>
       <Grid item xs={11} md={6} className={classes.row}>
