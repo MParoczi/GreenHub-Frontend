eac2a16ccc1d5c3afb04980516a62b8e77eb04e5
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-22 20:27:41 +0200

    Fix post reducer to store payload of the responses

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -4,9 +4,9 @@ import initialState from "./initialState";
 export default function postReducer(state = initialState.posts, action) {
   switch (action.type) {
     case types.ADD_POST_SUCCESS:
-      return [...state, action.post];
+      return [...state, action.post.payload];
     case types.GET_USER_POSTS_SUCCESS:
-      return action.posts;
+      return action.posts.payload;
     default:
       return state;
   }
