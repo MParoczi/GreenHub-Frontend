1abf19109abf2d46b0eb68d4712ea150949df302
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-22 18:57:43 +0200

    Add getUserPosts actions

--- src/redux/actions/postActions.js
+++ src/redux/actions/postActions.js
@@ -6,12 +6,29 @@ function addPostSuccess(post) {
   return { type: types.ADD_POST_SUCCESS, post };
 }
 
+function getUserPostsSuccess(posts) {
+  return { type: types.GET_USER_POSTS_SUCCESS, posts };
+}
+
 export function addPost(post, token) {
   return function(dispatch) {
     dispatch(beginApiCall());
     return postApi
       .addPost(post, token)
       .then(post => dispatch(addPostSuccess(post)))
+      .catch(error => {
+        dispatch(apiCallError());
+        throw error;
+      });
+  };
+}
+
+export function getUserPosts(user, token) {
+  return function(dispatch) {
+    dispatch(beginApiCall());
+    return postApi
+      .getUserPosts(user, token)
+      .then(posts => dispatch(getUserPostsSuccess(posts)))
       .catch(error => {
         dispatch(apiCallError());
         throw error;
