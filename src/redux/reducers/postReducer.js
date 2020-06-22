fc77387fc3767317196f39b092a08d9b56f8ffa9
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-22 18:58:56 +0200

    Handle get user posts in post reducer

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -5,6 +5,8 @@ export default function postReducer(state = initialState.posts, action) {
   switch (action.type) {
     case types.ADD_POST_SUCCESS:
       return [...state, action.post];
+    case types.GET_USER_POSTS_SUCCESS:
+      return action.posts;
     default:
       return state;
   }
