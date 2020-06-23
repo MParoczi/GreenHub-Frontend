c1edb8fcdf4dc8295ad92e0a36ffc5d93e89ce53
Márk Paróczi <mark.paroczi@gmail.com>
2020-06-23 20:48:45 +0200

    Handle post update in post reducer

--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -9,6 +9,10 @@ export default function postReducer(state = initialState.posts, action) {
       return action.posts.payload;
     case types.DELETE_POST_SUCCESS:
       return state.filter(post => post.id !== action.post.payload.id);
+    case types.UPDATE_POST_SUCCESS:
+      return state.map(post =>
+        post.id === action.post.payload ? action.post.payload : post
+      );
     default:
       return state;
   }
