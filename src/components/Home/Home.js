41feb0e6b801ea225e2570d0d6e3e360b8bd0f00
Márk Paróczi <mark.paroczi@gmail.com>
2020-07-02 08:50:02 +0200

    Fetch users in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import { useStyle } from "./homeStyle";
+import { getUsers } from "../../redux/actions/userActions";
 
 function Home() {
   const classes = useStyle();
+  const user = useSelector(state => state.loggedInUser);
+  const users = useSelector(state => state.users);
+  const dispatch = useDispatch();
+
+  const fetchUsers = useCallback(() => {
+    if (users.length === 0) {
+      dispatch(getUsers(user));
+    }
+  }, [dispatch, user, users.length]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
 
   return (
     <Grid container spacing={0} justify="center">
