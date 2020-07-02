66fece32a7a8c210e44160e5871aa2664fbe8f15
Márk Paróczi <mark.paroczi@gmail.com>
2020-07-02 08:39:51 +0200

    Add usersReducer

---
+++ src/redux/reducers/usersReducer.js
@@ -0,0 +1,11 @@
+import * as types from "../actions/actionTypes";
+import initialState from "./initialState";
+
+export default function usersReducer(state = initialState.users, action) {
+  switch (action.type) {
+    case types.GET_USERS_SUCCESS:
+      return action.users.payload;
+    default:
+      return state;
+  }
+}
