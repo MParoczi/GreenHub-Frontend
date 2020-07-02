f75563f7e2def37c3f6f3e73196d8fac4789b8c5
Márk Paróczi <mark.paroczi@gmail.com>
2020-07-02 10:29:30 +0200

    Redirect to selected user page

--- src/components/Home/UserThumbnail.js
+++ src/components/Home/UserThumbnail.js
@@ -1,23 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import PropTypes from "prop-types";
 import Chip from "@material-ui/core/Chip";
 import Avatar from "@material-ui/core/Avatar";
 import defaultProfilePic from "../../images/default_profile.png";
 
 function UserThumbnail({ user }) {
+  const history = useHistory();
+
+  const redirect = () => {
+    history.push(`/profile/${user.id}`);
+  };
+
   return (
-    <Link to={`/profile/${user.id}`}>
-      <Chip
-        variant="outlined"
-        label={`${user.firstName} ${user.lastName}`}
-        avatar={
-          <Avatar
-            src={user.profilePicture ? user.profilePicture : defaultProfilePic}
-          />
-        }
-      />
-    </Link>
+    <Chip
+      variant="outlined"
+      label={`${user.firstName} ${user.lastName}`}
+      onClick={redirect}
+      avatar={
+        <Avatar
+          src={user.profilePicture ? user.profilePicture : defaultProfilePic}
+        />
+      }
+    />
   );
 }
 
